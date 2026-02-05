389ff194d3ad9a3f1302db66bd98b70f07cd1a10
cassiebuhler <>
2026-02-05 19:45:11 +0000

    clean up

--- utils.py
+++ utils.py
@@ -97,3 +97,164 @@ def year_line(df, y, group, title, y_title, stat='percent'):
         )
         .properties(width=800, height=160)
     )
+
+
+def year_line_mechanism(df, y, group, title, y_title, stat='percent'):
+    finance_colors = alt.Scale(
+    # domain=['Property tax','Real estate transfer tax','Sales tax','Income tax','Tax (Other)'],
+    # domain=["Tax","Bond","Other"],
+    domain=["Tax","Bond"],
+
+    range=[
+        '#1f77b4',  # blue
+        '#ff7f0e',  # orange
+        # '#2ca02c',  # green
+        # '#d62728',  # red
+        # '#9467bd',  # purple
+        # '#8c564b',  # brown
+    ],
+    )
+
+    legend = alt.Legend(
+        title=None,
+        labelFontSize=14,
+        labelLimit=500,
+        orient='top',
+        direction='horizontal',
+        offset=5
+    )
+
+    if stat == 'percent':
+        y_axis = alt.Axis(format="%", labelFontSize=14, titleFontSize=18)
+    elif stat == 'count':
+        y_axis = alt.Axis(format="d", labelFontSize=14, titleFontSize=18)
+    else:
+        y_axis = alt.Axis(
+            format="$,.0f",
+            labelExpr="datum.value / 1000000",
+            labelFontSize=14,
+            titleFontSize=18,
+        )
+
+    x_axis = alt.Axis(
+        labelFontSize=14,
+        titleFontSize=18,
+        labelPadding=4,
+        titlePadding=10,
+        labelExpr="(toNumber(datum.value) % 2 === 0) ? datum.value : ''"
+    )
+
+    return (
+        alt.Chart(df, title=alt.TitleParams(text=title, fontSize=20, dy=-5))
+        .mark_line(point=True)
+        .encode(
+            x=alt.X("year:O", title="Year", axis=x_axis),
+            y=alt.Y(f"{y}:Q", title=y_title, axis=y_axis),
+            color=alt.Color("mechanism_group:N", scale=finance_colors, legend=legend),
+            # color=alt.Color("finance_mechanism:N", scale=finance_colors, legend=legend),
+
+        )
+        .properties(width=800, height=160)
+    )
+
+
+def bar_chart(df, y, group, title, y_title, stat='percent'):
+    legend = alt.Legend(
+        title=None,
+        labelFontSize=14,
+        labelLimit=500,
+        orient='top',
+        direction='horizontal',
+        offset=5
+    )
+
+    if stat == 'percent':
+        y_axis = alt.Axis(format="%", labelFontSize=14, titleFontSize=18)
+    elif stat == 'count':
+        y_axis = alt.Axis(format="d", labelFontSize=14, titleFontSize=18)
+    else:
+        y_axis = alt.Axis(
+            format="$,.0f",
+            labelExpr="datum.value / 1000000",
+            labelFontSize=14,
+            titleFontSize=18,
+        )
+
+    x_axis = alt.Axis(
+        labelFontSize=14,
+        titleFontSize=18,
+        labelPadding=4,
+        titlePadding=10,
+        # labelExpr="(toNumber(datum.value) % 2 === 0) ? datum.value : ''"
+    )
+
+    return (
+        alt.Chart(df, title=alt.TitleParams(text=title, fontSize=20, dy=-5))
+        .transform_calculate(
+            party_label=(
+                f"datum['{group}'] === 'Republican' ? '{LABEL_R}' : "
+                f"datum['{group}'] === 'Democrat' ? '{LABEL_D}' : "
+                f"datum['{group}']"
+            )
+        )
+        .mark_line(point=True)
+        .encode(
+            x=alt.X("mechanism_type:O", title="Finance Mechanism", axis=x_axis),
+            y=alt.Y(f"{y}:Q", title=y_title, axis=y_axis),
+            color=alt.Color("party_label:N", scale=party_colors, legend=legend),
+        )
+        .properties(width=800, height=160)
+    )
+
+def bar_chart(df, y, group, title, y_title, stat='percent'):
+    legend = alt.Legend(
+        title=None,
+        labelFontSize=14,
+        labelLimit=500,
+        orient='bottom',
+        direction='horizontal',
+        offset=5
+    )
+
+    if stat == 'percent':
+        y_axis = alt.Axis(format="%", labelFontSize=14, titleFontSize=18)
+    elif stat == 'count':
+        y_axis = alt.Axis(format="d", labelFontSize=14, titleFontSize=18)
+    else:
+        y_axis = alt.Axis(
+            format="$,.0f",
+            labelExpr="datum.value / 1000000",
+            labelFontSize=14,
+            titleFontSize=18,
+        )
+
+    x_axis = alt.Axis(
+        title=None,
+        labels=False
+    )
+    col_header = alt.Header(
+        title = None,
+         labelFontSize=18,
+    )
+
+    return (
+        alt.Chart(df, title=alt.TitleParams(text=title, fontSize=20, dy=-5))
+        .transform_calculate(
+            party_label=(
+                f"datum['{group}'] === 'Republican' ? '{LABEL_R}' : "
+                f"datum['{group}'] === 'Democrat' ? '{LABEL_D}' : "
+                f"datum['{group}']"
+            )
+        )
+        .mark_bar()
+        .encode(
+            column=alt.Column("mechanism_group:N", title="Finance Mechanism", header = col_header),
+            y=alt.Y(f"{y}:Q", title=y_title, axis=y_axis),
+            color=alt.Color("party_label:N", scale=party_colors, legend=legend),
+            x=alt.X("party_label:N", title=None, sort=[LABEL_D, LABEL_R],axis=x_axis),
+
+        )
+        .properties(width=300, height=160)
+    )
+
+## graphing utils 
